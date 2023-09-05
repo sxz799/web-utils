0247c36072477bcb12afb37fe913558eb8ed84a1
sxz799 <sxz799@126.com>
2023-09-05 15:01:54 +0800

    转为vite+ts1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 
 
 <template>
-  <Index/>
+  <el-row>
+    <el-col :span="4" :xs="0"/>
+    <el-col :span="16" :xs="24">
+      <el-tabs tab-position="top">
+        <el-tab-pane label="格式化SQL IN 条件">
+          <el-card >
+            <TransToSqlIn/>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="密码生成器">
+          <el-card >
+            <PasswordGenerator/>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="Base64">
+          <el-card >
+            <Base64Converter/>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="MD5计算器">
+          <el-card >
+            <MD5Calculator/>
+          </el-card>
+        </el-tab-pane>
+        <el-tab-pane label="时间戳工具">
+          <el-card >
+            <TimestampConverter/>
+          </el-card>
+        </el-tab-pane>
+      </el-tabs>
+    </el-col>
+    <el-col :span="4" :xs="0"/>
+  </el-row>
 </template>
 
 <script setup lang="ts">
-import Index from "./components/Index.vue";
+
+import TransToSqlIn from './components/TransToSqlIn.vue'
+import PasswordGenerator from './components/PasswordGenerator.vue'
+import Base64Converter from './components/Base64Converter.vue'
+import MD5Calculator from './components/MD5Calculator.vue'
+import TimestampConverter from './components/TimestampConverter.vue'
+
 </script>
 
 <style scoped>
